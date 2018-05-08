27622fb501e964f557dbaf3b2a749b42ddf7a8ee
Mizar <mizar.jp@gmail.com>
2018-05-09 01:12:22 +0900

    View.Win2Dx: sandbox

---
+++ MyShogi/View/Win2Dx/BoardForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyShogi.View.Win2Dx
+{
+    public partial class BoardForm : Form
+    {
+        public BoardForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
