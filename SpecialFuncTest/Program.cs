4a8b3e3866c96afd00602f006332dfcb42a6502d
Mizar <mizar.jp@gmail.com>
2018-09-21 04:44:05 +0900

    - SpecialFuncTestProj

---
+++ SpecialFuncTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShogi.Model.Test;
+
+namespace SpecialFuncTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SpecialFuncTest.TestAll();
+            //EloRatingTest.TestWinLossElo();
+            EloRatingTest.TestWinLossElo2();
+            ;
+        }
+    }
+}
